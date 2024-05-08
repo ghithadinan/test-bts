09c2b3391bbbc1353e680dbf5ebba24bffa0da54
ghitha.dinan <ghithadinanhs@gmail.com>
2024-05-08 11:53:00 +0700

    JwtAuthFilter import fixing

--- src/main/java/com/example/spring/test/bts/config/JwtAuthFilter.java
+++ src/main/java/com/example/spring/test/bts/config/JwtAuthFilter.java
@@ -1,6 +1,10 @@
 package com.example.spring.test.bts.config;
 
 import com.example.spring.test.bts.service.UserInfoService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,7 +25,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private UserInfoService userDetailsService;
 
     @Override
-    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
